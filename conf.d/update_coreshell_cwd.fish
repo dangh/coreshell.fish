e71613a8716eafb18a412b624bf837bc9b9981ef
Dang <huynhminhdang@gmail.com>
2022-09-19 10:07:26 +0700

    use short flags

--- conf.d/update_coreshell_cwd.fish
+++ conf.d/update_coreshell_cwd.fish
@@ -1,10 +1,10 @@
-function update_coreshell_cwd --argument-names cwd --on-event fish_prompt
+function update_coreshell_cwd -a cwd -e fish_prompt
   # Identify the directory using a "file:" scheme URL, including
   # the host name to disambiguate local vs. remote paths.
 
   # Percent-encode the pathname.
-  test -n "$cwd" || set --local cwd (pwd)
-  set --local url_path (string escape --style=url $cwd)
+  test -n "$cwd" || set -l cwd (pwd)
+  set -l url_path (string escape --style=url $cwd)
 
   if test -n "$TMUX"
     printf '\ePtmux;\e\e]7;%s\a\e\\' "file://""$HOSTNAME""$url_path"
@@ -14,6 +14,6 @@ function update_coreshell_cwd --argument-names cwd --on-event fish_prompt
 end
 
 if test -n "$TMUX"
-  set --local fish (which fish)
+  set -l fish (which fish)
   tmux set-hook -g window-pane-changed 'run-shell "'$fish' -c \'update_coreshell_cwd \'#{pane_current_path}\'\'"'
 end
